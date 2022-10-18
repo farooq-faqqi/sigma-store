0834957d79b83fd47ec0f63cb350ddc7b40e4c81
farooq-faqqi <farooq.faqqi@gmail.com>
2022-10-18 17:24:19 +0500

    this is the first try

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
     defaultLanguageCode: 'EN',
     storeDomain:
       // @ts-ignore
-      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'hydrogen-preview.myshopify.com',
+      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'sigmastore-2694.myshopify.com/',
     storefrontToken:
       // @ts-ignore
       Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN ||
-      '3b580e70970c4528da70c98e097c2fa0',
+      '1298cfccf14dfc615991cbfebaa82749',
     privateStorefrontToken:
       // @ts-ignore
       Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
